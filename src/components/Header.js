641582a4b27390b98f34564ccf0e1a0880005ba4
Álvaro Fernández García <afernandezgarcia@serbatic.es>
2020-05-12 22:31:34 +0200

    prettier config

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Link, withRouter } from 'react-router-dom';
-import { injectIntl } from 'react-intl';
-import { ROUTES } from 'config/constants';
+import {Link, withRouter} from 'react-router-dom';
+import {injectIntl} from 'react-intl';
+import {ROUTES} from 'config/constants';
 import CustomImage from './CustomImage';
 import cactusImage from 'assets/cactus.svg';
 import 'style/header.css';
 
-const Header = ({ language, location, intl, onChangeLanguage }) => {
+const Header = ({language, location, intl, onChangeLanguage}) => {
   return (
     <div id="header">
       <Link to={ROUTES.HOME}>
@@ -14,10 +14,10 @@ const Header = ({ language, location, intl, onChangeLanguage }) => {
       </Link>
       <div>
         <Link className={location.pathname === ROUTES.HOME ? 'active' : ''} to={ROUTES.HOME}>
-          {intl.formatMessage({ id: 'header.home' })}
+          {intl.formatMessage({id: 'header.home'})}
         </Link>
         <Link className={location.pathname === ROUTES.STORE ? 'active' : ''} to={ROUTES.STORE}>
-          {intl.formatMessage({ id: 'header.store' })}
+          {intl.formatMessage({id: 'header.store'})}
         </Link>
       </div>
       <span onClick={onChangeLanguage}>{language}</span>
